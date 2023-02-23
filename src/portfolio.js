72ada96a58f4dceadcbefb0f3354b62adb503a07
Varun Bhartiya <vpbhartiya5@gmail.com>
2023-02-23 18:15:42 +0530

    new projects added

--- src/portfolio.js
+++ src/portfolio.js
@@ -11,15 +11,39 @@ const about = {
 
 const projects = [
   {
+    name: "QTrip Static",
+    description:
+      "This project is a Static traveling  website for booking tickets for resorts from many cities having various adventures",
+    stack: ["HTML", "CSS", "Bootstrap"],
+    sourceCode: "https://github.com/vpbhartiya5/QTrip_Static"
+  },
+
+  {
+    name: "QTrip Dynamic",
+    description:
+      "This project is a Dynamic traveling  website for booking tickets from many cities consisting of various adventures",
+    stack: ["HTML", "CSS", "Bootstrap","JavaScript", "REST API"],
+    sourceCode: "https://github.com/vpbhartiya5/QTrip_Dynamic"
+  },
+
+  {
     name: "Grocery Store",
     description:
       "This project is a SPA for shopping Groceries built using React along with React Router, Redux and Semantic UI",
-    stack: ["HTML", "CSS", "Javascript", "React", "Redux"],
-    sourceCode: "https://github.com/vpbhartiya5"
+    stack: ["HTML", "CSS", "JavaScript", "React", "Redux", "Semantic UI"],
+    sourceCode: "https://github.com/vpbhartiya5/grocery-store"
+  },
+
+  {
+    name: "XBoard",
+    description:
+      "This project is a News Feed website which aggregates news content from different websites in one place. It consists of news articles grouped under various topics such as Technology, Politics, Sports etc.",
+    stack: ["HTML", "CSS", "Bootstrap","JavaScript", "REST API"],
+    sourceCode: "https://github.com/vpbhartiya5/XBoard"
   }
 ];
 
-const skills = ["HTML", "CSS", "JavaScript", "React", "Redux", "Semantic UI"];
+const skills = ["HTML", "CSS", "Bootstrap", "JavaScript", "React", "Redux", "REST API", "Semantic UI", "Material UI"];
 
 const contact = {
   email: "vpbhartiya5@gmail.com"
