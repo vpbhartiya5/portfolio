3e6b6db1480a9ac00f4a5887b6e5688edb5380d5
Varun Bhartiya <vpbhartiya5@gmail.com>
2023-04-04 15:13:15 +0530

    portfolio modified

--- src/portfolio.js
+++ src/portfolio.js
@@ -2,7 +2,7 @@ const about = {
   name: "Varun Bhartiya",
   role: "Frontend Developer in React",
   description:
-    "Varun is a Frontend Developer in React. He has straightforward communication and advanced English; moreover, and is a sympathetic down-to-earth person. Furthermore, he is familiar with working with best developing practices to enhance product development (DRY principle, modularised code).",
+    "Varun is a skilled Frontend Developer in React. He has straightforward communication and advanced English; moreover, and is a sympathetic down-to-earth person. Furthermore, he is familiar with working with best developing practices to enhance product development (DRY principle, modularised code).",
   social: {
     linkedin: "https://www.linkedin.com/in/varun-pramod-bhartiya-09472/",
     github: "https://github.com/vpbhartiya5"
@@ -10,6 +10,14 @@ const about = {
 };
 
 const projects = [
+  {
+    name: "QKart",
+    description:
+      "This project is an e-commerce application offering a variety of products for customers to choose from. Have implemented the core logic for authentication, shopping cart and checkout.",
+    stack: ["React", "React Router V5", "React Hooks", "Debouncing", "Material UI", "REST API"],
+    sourceCode: "https://github.com/vpbhartiya5/QKart"
+  },
+
   {
     name: "QTrip Static",
     description:
@@ -21,8 +29,8 @@ const projects = [
   {
     name: "QTrip Dynamic",
     description:
-      "This project is a Dynamic traveling  website for booking tickets from many cities consisting of various adventures",
-    stack: ["HTML", "CSS", "Bootstrap","JavaScript", "REST API"],
+      "This project is a Dynamic traveling  website aimed at travelers looking for a multitude of adventures in different cities.",
+    stack: ["HTML", "CSS", "Bootstrap","JavaScript", "DOM", "REST API", "JSON"],
     sourceCode: "https://github.com/vpbhartiya5/QTrip_Dynamic"
   },
 
@@ -38,12 +46,12 @@ const projects = [
     name: "XBoard",
     description:
       "This project is a News Feed website which aggregates news content from different websites in one place. It consists of news articles grouped under various topics such as Technology, Politics, Sports etc.",
-    stack: ["HTML", "CSS", "Bootstrap","JavaScript", "REST API"],
+    stack: ["HTML", "CSS", "Bootstrap","JavaScript", "Figma"],
     sourceCode: "https://github.com/vpbhartiya5/XBoard"
   }
 ];
 
-const skills = ["HTML", "CSS", "Bootstrap", "JavaScript", "React", "Redux", "REST API", "Semantic UI", "Material UI"];
+const skills = ["HTML", "CSS", "Bootstrap", "JavaScript", "React", "Redux", "React Router", "REST API", "DevTools", "Material UI"];
 
 const contact = {
   email: "vpbhartiya5@gmail.com"
